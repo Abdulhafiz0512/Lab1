2767980dd5a76920d3742d605ba08f4de940b330
Abdulhafiz <a.yoqubjonov@gmail.com>
2024-03-06 23:56:15 +0500

    anakd

--- problem4.cpp
+++ problem4.cpp
@@ -3,4 +3,6 @@ using namespace std;
 int main(){
     cout<<"I'm sleeping\nRunning!\nI'm sleeping\nRunning!\nI'm sleeping\nRunning!\nI'm sleeping\nRunning!\nI'm sleeping\nRunning!\nI'm eating\nBark!\n40";
     return 0;
+
+
 }
