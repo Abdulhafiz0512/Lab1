1c672a8f6976f116b19286e4b5ebaa6e6de3ecc2
Abdulhafiz <a.yoqubjonov@gmail.com>
2024-03-06 23:55:56 +0500

    anakd

---
+++ problem4.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"I'm sleeping\nRunning!\nI'm sleeping\nRunning!\nI'm sleeping\nRunning!\nI'm sleeping\nRunning!\nI'm sleeping\nRunning!\nI'm eating\nBark!\n40";
+    return 0;
+}
