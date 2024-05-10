83299dcd9f42b1b878eebdb801fcfc9c127069c8
Abdulhafiz Yoqubjonov <147692080+Abdulhafiz0512@users.noreply.github.com>
2024-05-10 16:17:58 +0500

    Update problem2.cpp

--- problem2.cpp
+++ problem2.cpp
@@ -1,36 +1,104 @@
 #include <iostream>
-#include <vector>
+#include <queue>
 using namespace std;
-int main() {
-    int n, m;
-    cin >> n >> m;
 
-    vector<vector<int>> graph(n);
+class Node {
+public:
+    int key;
+    Node* left;
+    Node* right;
+    int height;
 
-    for (int i = 0; i < m; i++) {
-        int u, v;
-        cin >> u >> v;
-        graph[u].push_back(v);
-        graph[v].push_back(u);
+    Node(int k) : key(k), left(nullptr), right(nullptr), height(1) {}
+};
+
+class AVLTree {
+public:
+    Node* root;
+
+    AVLTree() : root(nullptr) {}
+
+    // Function to calculate the sum of all leaves in the AVL tree
+    int sumOfLeaves(Node* node) {
+        if (!node) {
+            return 0;
+        }
+        if (!node->left && !node->right) {
+            return node->key;
+        }
+        return sumOfLeaves(node->left) + sumOfLeaves(node->right);
     }
-    int q;
-    cin >> q;
 
-    for (int i = 0; i < q; i++) {
-        int u, v;
-        cin >> u >> v;
-        bool found = false;
-        for (int j = 0; j < graph[u].size(); j++) {
-            if (graph[u][j] == v) {
-                found = true;
-                break;
+    // Function to perform level order traversal of the AVL tree
+    void levelOrderTraversal(Node* root) {
+        queue<Node*> q;
+        
+        if (!root) {
+            return;
+        }
+        
+        q.push(root);
+        
+        while (!q.empty()) {
+            Node* node = q.front();
+            q.pop();
+            
+            if (!node->left && !node->right) {
+                cout << node->key << " ";
+            }
+            
+            if (node->left) {
+                q.push(node->left);
+            }
+            
+            if (node->right) {
+                q.push(node->right);
             }
         }
-        if (found) {
-            cout << "YES" << endl;
+    }
+};
+
+int main() {
+    AVLTree avlTree;
+
+    int n, element;
+
+    cout << "Enter the number of nodes in the AVL tree: ";
+    cin >> n;
+
+    cout << "Enter the elements in level order traversal:" << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> element;
+        if (i == 0) {
+            avlTree.root = new Node(element);
         } else {
-            cout << "NO" << endl;
+            queue<Node*> q;
+            q.push(avlTree.root);
+
+            while (!q.empty()) {
+                Node* curr = q.front();
+                q.pop();
+
+                if (!curr->left) {
+                    curr->left = new Node(element);
+                    break;
+                } else {
+                    q.push(curr->left);
+                }
+
+                if (!curr->right) {
+                    curr->right = new Node(element);
+                    break;
+                } else {
+                    q.push(curr->right);
+                }
+            }
         }
     }
+
+    cout  << avlTree.sumOfLeaves(avlTree.root) << endl;
+
+    
+
     return 0;
 }
